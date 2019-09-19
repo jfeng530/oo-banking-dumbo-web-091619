1c8eea55b3544f8cdbd00f731c65283abb0fdd15
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-19 21:46:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if self.valid? && self.sender.balance > self.amount
+    if self.valid? && self.sender.balance > self.amount && self.status == "pending"
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
@@ -29,9 +29,9 @@ class Transfer
   end 
   
   def reverse_transfer
+    
     self.sender.balance += self.amount
     self.receiver.balance -= self.amount
-    self.amount = 0 
   end
   
 end
