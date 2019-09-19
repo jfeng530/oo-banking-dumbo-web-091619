6ac5411d6c1483bf5943fcd2edf599074e51b547
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-19 21:46:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,9 +29,9 @@ class Transfer
   end 
   
   def reverse_transfer
-    
     self.sender.balance += self.amount
     self.receiver.balance -= self.amount
+    self.status = "reversed"
   end
   
 end
